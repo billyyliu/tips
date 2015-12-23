c367a7a9ef46bc3ce984f5b8cfcc191485cb1bd0
Ziyan Liu <lzyzy1996@gmail.com>
2015-12-23 09:31:03 +0800

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Ziyan on 12/23/15.
+//  Copyright © 2015 Ziyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
